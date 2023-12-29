300435147a8fe22b9fc5b7ad2f703eb292b94399
Caudate7855 <118586343+Caudate7855@users.noreply.github.com>
2023-12-29 17:52:15 +0700

    Time: 173 ms (5.99%), Space: 53.1 MB (100.00%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cs
@@ -0,0 +1,34 @@
+public class Solution 
+{
+    public List<string> FizzBuzz(int n) 
+    {
+        List<string> answer = new List<string>(n);
+        
+        for( int i = 1; i <= n; i++)
+        {
+            bool divisibleBy3 = i % 3 == 0;
+            bool divisibleBy5 = i % 5 == 0;
+            
+            
+            if(divisibleBy3 && divisibleBy5)
+            {
+                answer.Add("FizzBuzz");
+            }
+            else if(divisibleBy3)
+            {
+                answer.Add("Fizz");
+            }
+            else if(divisibleBy5)
+            {
+                answer.Add("Buzz");
+            }
+            
+            else
+            {
+                answer.Add(i.ToString());
+            }
+        }
+        
+        return answer;
+    }
+}
