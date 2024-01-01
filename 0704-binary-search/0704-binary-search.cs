3bd6092827d73cb0f0dd49f7c30274a8891aa6b8
Caudate7855 <118586343+Caudate7855@users.noreply.github.com>
2024-01-02 02:20:54 +0700

    Time: 182 ms (5.03%), Space: 48.4 MB (96.44%) - LeetHub

--- 0704-binary-search/0704-binary-search.cs
+++ 0704-binary-search/0704-binary-search.cs
@@ -1,14 +1,28 @@
-public class Solution {
+public class Solution 
+{
     public int Search(int[] nums, int target) 
     {
-            for(int i = 0; i < nums.Length; i++)
-            {
-                if(nums[i] == target)
-                {
-                    return i; 
-                }
-               
-            } 
-        return -1;
+        int left = 0;
+        int right = nums.Length - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid; 
+        }
+        else if (nums[mid] < target)
+        {
+            left = mid + 1; 
+        }
+        else
+        {
+            right = mid - 1; 
+        }
     }
+
+    return -1; 
+}
 }
